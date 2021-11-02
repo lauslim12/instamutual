bd333202a56fea3edd212ffbdc462765ad7db9cb
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2021-11-02 14:07:58 +0700

    Implement utility function to get environment variable

---
+++ src/utils.ts
@@ -0,0 +1,15 @@
+/**
+ * Returns value stored in environment variable with the given 'name'.
+ * Throws error if no such variable or if variable undefined; thus ensuring type-safety.
+ *
+ * @param name - name of variable to fetch from this process's environment.
+ */
+export function env(name: string): string {
+  const value = process.env[name];
+
+  if (!value) {
+    throw new Error(`Missing environment variable: '${name}'.`);
+  }
+
+  return value;
+}
