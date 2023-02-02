7d44f30550abcead6e65b855cc7e5d32645cbd61
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2023-02-02 23:31:24 +0900

    Add delay function

--- src/utils.ts
+++ src/utils.ts
@@ -2,9 +2,9 @@
  * Returns value stored in environment variable with the given 'name'.
  * Throws error if no such variable or if variable undefined; thus ensuring type-safety.
  *
- * @param name - Name of variable to fetch from this process's environment.
+ * @param name - Name of variable to fetch from this process's environment
  */
-export function env(name: string): string {
+export function env(name: string) {
   const value = process.env[name];
 
   if (!value) {
@@ -12,4 +12,14 @@ export function env(name: string): string {
   }
 
   return value;
+}
+
+/**
+ * Artificial delays, useful to prevent getting rate-limited.
+ *
+ * @param ms - Milliseconds to wait
+ * @returns A resolved promise after `ms` milliseconds
+ */
+export async function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
