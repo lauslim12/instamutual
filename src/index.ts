04c0c578626fe0f7744c47c079cd0c2766a69921
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2023-07-14 22:18:23 +0900

    Move schema to its own file

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,10 @@
-import { z } from 'zod';
-
 import {
   createOutputData,
   getFileWithDefault,
   readInputFromFile,
   writeOutputToFile,
 } from './file';
-
-export const relationshipSchema = z
-  .array(
-    z
-      .object({ string_list_data: z.array(z.object({ value: z.string() })) })
-      .transform(({ string_list_data }) => {
-        // In an unlikely scenario that the data is not there, mark it as undefined.
-        // We will strip it separately in the next step.
-        if (!string_list_data[0]) {
-          return undefined;
-        }
-
-        return string_list_data[0].value;
-      }),
-  )
-  .transform((arg) =>
-    // Have to use type guard (not type assertion) to tell TypeScript that this is really a string.
-    // https://www.benmvp.com/blog/filtering-undefined-elements-from-array-typescript/
-    arg.filter((data): data is string => typeof data !== undefined),
-  );
-
-export const followingSchema = z
-  .object({ relationships_following: relationshipSchema })
-  .transform((schema) => schema.relationships_following);
+import { followingSchema, relationshipSchema } from './schema';
 
 /**
  * Driver code to run the whole project.
